058c889690c36c5356e4e8bffb102c26ba93d053
Alexandre <116552630+Creepynax@users.noreply.github.com>
2023-01-04 14:50:58 +0100

    getter and setter

--- src/main/java/com/codingf/tictactoe/square/Square.java
+++ src/main/java/com/codingf/tictactoe/square/Square.java
@@ -1,5 +1,13 @@
 package com.codingf.tictactoe.square;
 
 public class Square {
+    private String player = null;
 
+    public String getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(String player) {
+        this.player = player;
+    }
 }
