21a85fa21d279d9453c7747bf696dee440e3ad20
raphaelglt <raphael.guillot@edu.esiee-it.fr>
2023-01-05 09:09:52 +0100

    check if win

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,67 @@
 package org.example;
+import com.condingf.tictactoe.board.Board;
+
 import java.util.Scanner;
 public class Main {
 
+    private static boolean endOfGameCheck(int boardSize, Board board) {
+        //regarde si toutes les cases sont prises
+        int numberOfUntaken = 0;
+        for (int i = 0; i<boardSize; i++) {
+            for (int j = 0; j<boardSize; j++) {
+                if (board.getBoard().get(i).get(j).getPlayer() == 0) {
+                    numberOfUntaken++;
+                }
+            }
+        }
+        if(numberOfUntaken==0) return false;
+
+
+        //vérifie les colonnes
+        for (int i = 1; i<boardSize; i++) {
+            int numberOfSame = 0;
+            for (int j = 0; j<boardSize; j++) {
+                if (board.getBoard().get(i).get(j).getPlayer() == board.getBoard().get(i-1).get(j).getPlayer() && board.getBoard().get(i).get(j).getPlayer() != 0) {
+                    numberOfSame++;
+                }
+            }
+            if (numberOfSame+1 == boardSize) return false;
+        }
+
+
+        //vérifie les lignes
+        for (int i = 0; i<boardSize; i++) {
+            int numberOfSame = 0;
+            for (int j = 1; j<boardSize; j++) {
+                if (board.getBoard().get(i).get(j).getPlayer() == board.getBoard().get(i).get(j-1).getPlayer() && board.getBoard().get(i).get(j).getPlayer() != 0) {
+                    numberOfSame++;
+                }
+            }
+            if (numberOfSame+1 == boardSize) return false;
+        }
+
+
+        //vérifie la diagonale gauche
+        int numberOfSame = 0;
+        for (int i = 0; i<boardSize-1; i++) {
+            if (board.getBoard().get(i).get(i).getPlayer() == board.getBoard().get(i).get(i+1).getPlayer() && board.getBoard().get(i).get(i).getPlayer() != 0) {
+                numberOfSame++;
+            }
+        }
+        if (numberOfSame == boardSize) return false;
+
+
+        //vérifie la diagonale droite
+        numberOfSame = 0;
+        int boardLengthSize = boardSize-1;
+        for (int i = 0; i<boardSize; i++) {
+            if (board.getBoard().get(i).get(boardLengthSize-i).getPlayer() == board.getBoard().get(boardLengthSize-i).get(i).getPlayer() && board.getBoard().get(boardLengthSize-i).get(i).getPlayer() != 0) {
+
+                numberOfSame++;
+            }
+        }
+        return numberOfSame != boardSize;
+    }
     public static void main(String[] args) {
         Scanner boardSize = new Scanner(System.in);
         System.out.println("Please enter the board size ");
@@ -11,11 +71,16 @@ public class Main {
         }else {
             System.out.println("Enjoy dear");
         }
+
         boolean play = true;
+        Board board = new Board(ent);
+        System.out.println((board));
+        play = endOfGameCheck(ent, board);
+        System.out.println(play);
 
-        while (play){
-
-        }
+        /**while (play){
+*
+        }*/
 
     }
 }
