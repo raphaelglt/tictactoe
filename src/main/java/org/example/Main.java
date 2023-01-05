39686c1cbdaccc2d0fabf8b609a8c9ea62cbf350
Alexandre <116552630+Creepynax@users.noreply.github.com>
2023-01-05 10:09:23 +0100

    Perfectionnement

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner boardSize = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         boolean validation = false;
         int ent = 0;
         while (validation == false) {
             System.out.println("Please enter the board size ");
-            ent = boardSize.nextInt();
+            ent = input.nextInt();
             if (ent < 3 || ent > 10) {
                 System.out.println("Please enter number between 3 and 10");
             } else {
@@ -21,16 +21,16 @@ public class Main {
 
         Board board = new Board(ent);
         boolean play = true;
+        int actualPlayer = 1;
         while (play) {
             boolean validPlacement = false;
             while (validPlacement == false) {
-                Scanner ligne = new Scanner(System.in);
                 boolean validLine = false;
                 int numLine = 0;
                 while (validLine == false) {
                     System.out.println("Which Line ? : ");
-                    numLine = boardSize.nextInt();
-                    if (numLine < 0 || numLine > ent) {
+                    numLine = input.nextInt();
+                    if (numLine < 0 || numLine >= ent) {
                         System.out.println("Take a line between 0 and " + ent);
                     } else {
                         validLine = true;
@@ -38,14 +38,12 @@ public class Main {
                 }
 
 
-
-                Scanner colonne = new Scanner(System.in);
                 boolean validColumn = false;
                 int numColumn = 0;
                 while (validColumn == false) {
                     System.out.println("Which column ? : ");
-                    numColumn = boardSize.nextInt();
-                    if (numColumn < 0 || numColumn > ent) {
+                    numColumn = input.nextInt();
+                    if (numColumn < 0 || numColumn >= ent) {
                         System.out.println("Take a column between 0 and " + ent);
                     } else {
                         validColumn = true;
@@ -55,8 +53,15 @@ public class Main {
                     System.out.println("There is already something on this square");
                 }
                 else{
-                    board.getBoard().get(numLine).get(numColumn).setPlayer(1);
+                    board.getBoard().get(numLine).get(numColumn).setPlayer(actualPlayer);
                     System.out.println(board);
+                    if (actualPlayer == 1) {
+                        actualPlayer = 2;
+                    } else if (actualPlayer == 2) {
+                        actualPlayer = 1;
+                    } else {
+                        System.out.println("Player's number unknown");
+                    }
                     validPlacement = true;
                 }
             }
